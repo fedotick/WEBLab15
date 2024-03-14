c74d65b7a59f5d32904a5664d3278e28e5675ab3
fedotick <feodortcaciuc2004@gmail.com>
2024-03-14 14:06:37 +0200

    Task 4 complited

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,16 @@ alert(hello2());
 let mul = (n, m) => n * m;
 
 alert(mul(2, 3));
+
+// 4
+function kvadrat(x) {
+    return Math.pow(x, 2);
+}
+
+function kub(x) {
+    return Math.pow(x, 3);
+}
+
+let calc = (x, kvadrat, kub) => kvadrat(x) + kub(x);
+
+alert(calc(2, kvadrat, kub));
